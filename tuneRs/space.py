b1348c15da063057cf586901d93e655c888df0d9
Shane Stephenson <stephenson.shane.a@gmail.com>
2019-09-23 23:20:26 -0400

    cleaned up

--- tuneRs/space.py
+++ tuneRs/space.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class Uniform:
 
@@ -10,8 +11,6 @@ class Uniform:
         :param bins: Bins for numbers.  Each bin has equal chance of being pulled.  If None, no bins are used
         :param dtype: Dtype for distribution.  "int", "float", and "float32" are all valid inputs.
         '''
-        import numpy as np
-        self.np = np
         self.lower = lower
         self.upper = upper
         self.dtype = dtype
@@ -19,14 +18,14 @@ class Uniform:
 
     def _single_rvs(self, lower, upper, random_state=None):
         if random_state is None:
-            random_state = self.np.random.randint(0, 36e7)
-        self.np.random.seed(random_state)
+            random_state = np.random.randint(0, 36e7)
+        np.random.seed(random_state)
         if self.dtype == "int":
-            return self.np.random.randint(lower, upper+1)
+            return np.random.randint(lower, upper+1)
         elif self.dtype == "float":
-            return self.np.random.uniform(lower, upper)
+            return np.random.uniform(lower, upper)
         elif self.dtype == "float32":
-            return self.np.random.uniform(lower, upper).astype(self.np.float32)
+            return np.random.uniform(lower, upper).astype(np.float32)
 
     def rvs(self, num_samples, random_state=None):
         '''
@@ -37,21 +36,21 @@ class Uniform:
         :return: List of samples
         '''
         if random_state is None:
-            random_state = self.np.random.randint(0, 36e7)
-        self.np.random.seed(random_state)
+            random_state = np.random.randint(0, 36e7)
+        np.random.seed(random_state)
         if self.bins is None:
             if self.dtype=="int":
-                return self.np.random.randint(self.lower, self.upper+1, num_samples)
+                return np.random.randint(self.lower, self.upper+1, num_samples)
             elif self.dtype=="float":
-                return self.np.random.uniform(self.lower, self.upper, num_samples)
+                return np.random.uniform(self.lower, self.upper, num_samples)
             elif self.dtype=="float32":
-                return self.np.random.uniform(self.lower, self.upper, num_samples).astype(self.np.float32)
+                return np.random.uniform(self.lower, self.upper, num_samples).astype(np.float32)
         else:
             random_list = []
             num_bins = len(self.bins)
-            rand_list = self.np.random.randint(0, 36e7, num_samples)
+            rand_list = np.random.randint(0, 36e7, num_samples)
             for num in range(num_samples):
-                bin_range = self.bins[self.np.random.randint(0, num_bins)]
+                bin_range = self.bins[np.random.randint(0, num_bins)]
                 random_list.append(self._single_rvs(bin_range[0], bin_range[1], random_state=rand_list[num]))
             return random_list
 
@@ -67,24 +66,24 @@ class Normal:
         :param min: Minimum allowable sample.  If None, no min is considered
         :param dtype: Dtype for distribution.  "int", "float", and "float32" are all valid inputs.
         '''
-        import numpy as np
-        self.np = np
         self.mean = mean
         self.sd = sd
         self.dtype = dtype
         self.max = max
         self.min = min
+        self.lower = max # added for compatibility with Bayesian search methods
+        self.upper = min # added for compatibility with Bayesian search methods
 
     def _single_rvs(self, random_state=None):
         if random_state is None:
-            random_state = self.np.random.randint(0, 36e7)
-        self.np.random.seed(random_state)
+            random_state = np.random.randint(0, 36e7)
+        np.random.seed(random_state)
         if self.dtype == "int":
-            num = int(self.np.random.normal(self.mean, self.sd))
+            num = int(np.random.normal(self.mean, self.sd))
         elif self.dtype == "float":
-            num = self.np.random.normal(self.mean, self.sd)
+            num = np.random.normal(self.mean, self.sd)
         elif self.dtype == "float32":
-            num = self.np.float32(self.np.random.normal(self.mean, self.sd))
+            num = np.float32(np.random.normal(self.mean, self.sd))
         if self.min and self.max:
             if (self.min <= num) and (num <= self.max): return num
             else: return self._single_rvs(random_state=random_state + 1)
@@ -97,18 +96,18 @@ class Normal:
 
     def rvs(self, num_samples, random_state=None):
         if random_state is None:
-            random_state = self.np.random.randint(0, 36e7)
-        self.np.random.seed(random_state)
+            random_state = np.random.randint(0, 36e7)
+        np.random.seed(random_state)
         if (self.max is None) and (self.min is None):
             if self.dtype=="int":
-                return int(self.np.random.normal(self.mean, self.sd, num_samples))
+                return int(np.random.normal(self.mean, self.sd, num_samples))
             elif self.dtype=="float":
-                return self.np.random.normal(self.mean, self.sd, num_samples)
+                return np.random.normal(self.mean, self.sd, num_samples)
             elif self.dtype=="float32":
-                return np.float32(self.np.random.normal(self.mean, self.sd, num_samples))
+                return np.float32(np.random.normal(self.mean, self.sd, num_samples))
         else:
             random_list = []
-            rand_list = self.np.random.randint(0, 36e7, num_samples)
+            rand_list = np.random.randint(0, 36e7, num_samples)
             for index in range(num_samples):
                 random_list.append(self._single_rvs(random_state=rand_list[index]))
             return random_list
@@ -126,8 +125,6 @@ class LogNormal:
         :param reverse: True to have the distribution go from upper to lower instead
         :param dtype: Dtype for distribution.  "int", "float", and "float32" are all valid inputs.
         '''
-        import numpy as np
-        self.np = np
         self.lower = lower
         self.upper = upper
         self.dtype = dtype
@@ -137,17 +134,17 @@ class LogNormal:
 
     def rvs(self, num_samples, random_state=None):
         if random_state is None:
-            random_state = self.np.random.randint(0, 36e7)
-        self.np.random.seed(random_state)
+            random_state = np.random.randint(0, 36e7)
+        np.random.seed(random_state)
         if self.dtype == "int":
-            dist = self.np.random.choice(self.np.geomspace(self.lower, self.upper, num_samples*self.granularity, dtype=int),
+            dist = np.random.choice(np.geomspace(self.lower, self.upper, num_samples*self.granularity, dtype=int),
                                           num_samples, replace=self.replace)
         elif self.dtype == "float":
-            dist = self.np.random.choice(self.np.geomspace(self.lower, self.upper, num_samples*self.granularity), num_samples,
+            dist = np.random.choice(np.geomspace(self.lower, self.upper, num_samples*self.granularity), num_samples,
                                          replace=self.replace)
         elif self.dtype == "float32":
-            dist = self.np.random.choice(self.np.geomspace(self.lower, self.upper, num_samples*self.granularity,
-                                                            dtype=self.np.float32), num_samples, replace=self.replace)
+            dist = np.random.choice(np.geomspace(self.lower, self.upper, num_samples*self.granularity,
+                                                            dtype=np.float32), num_samples, replace=self.replace)
         if self.reverse:
             dist = [(self.upper - num) for num in dist]
         return dist
@@ -162,25 +159,29 @@ class Categorical:
         :param probs: list of probabilities for each category.  If None, all categories have equal cance of being sampled
         :param replace: True to allow the same category to be sampled multiple times
         '''
-        import numpy as np
-        self.np = np
         self.categories = categories
         self.cat_index = list(range(len(categories)))
         if probs:
+            self.default_probs = False
             self.probs = probs/np.sum(probs)
         else:
+            self.default_probs = True
             self.probs = [1/len(categories)]*len(categories)
         self.replace = replace
 
     def rvs(self, num_samples, random_state=None):
         if random_state is None:
-            random_state = self.np.random.randint(0, 36e7)
-        self.np.random.seed(random_state)
-        index_dist = self.np.random.choice(self.cat_index, num_samples, replace=self.replace, p=self.probs)
+            random_state = np.random.randint(0, 36e7)
+        np.random.seed(random_state)
+        index_dist = np.random.choice(self.cat_index, num_samples, replace=self.replace, p=self.probs)
         return [self.categories[index] for index in index_dist]
 
     def __add__(self, other_cat):
-        return Categorical(self.categories+other_cat.categories, self.probs+other_cat.probs, replace = (self.replace and other_cat.replace))
+        if self.default_probs and other_cat.default_probs:
+            return Categorical(self.categories+other_cat.categories, replace=(self.replace and other_cat.replace))
+        else:
+            return Categorical(self.categories+other_cat.categories, self.probs+other_cat.probs,
+                               replace=(self.replace and other_cat.replace))
 
 class Concatenate:
 
@@ -189,32 +190,38 @@ class Concatenate:
         Concatenate two distributions together
 
         :param categories: List of distributions
-        :param probs: list of probabilities for each distribution.  If None, all categories have equal chance of being sampled
+        :param probs: list of probabilities for each distribution.  If None, all categories have equal chance of
+            being sampled
         '''
-        import numpy as np
-        self.np = np
         self.categories = categories
         if probs:
+            self.default_probs = False
             self.probs = probs/np.sum(probs)
         else:
+            self.default_probs = True
             self.probs = [1/len(dist_list)]*len(dist_list)
 
     def rvs(self, num_samples, random_state=None):
         if random_state is None:
-            random_state = self.np.random.randint(0, 36e7)
-        self.np.random.seed(random_state)
-        rand_list = self.np.random.randint(0, 36e7, num_samples)
-        samples = self.np.random.choice(self.categories, num_samples, replace=True, p=self.probs)
+            random_state = np.random.randint(0, 36e7)
+        np.random.seed(random_state)
+        rand_list = np.random.randint(0, 36e7, num_samples)
+        samples = np.random.choice(self.categories, num_samples, replace=True, p=self.probs)
         sampled_samples = []
         for index, sample in enumerate(samples):
             sampled_samples.append(sample.rvs(1, random_state=rand_list[index])[0])
         return sampled_samples
 
     def __add__(self, other_dist):
-        return Concatenate(self.categories+other_dist.categories, self.probs+other_dist.probs)
+        if self.default_probs and other_dist.default_probs:
+            return Concatenate(self.categories+other_dist.categories)
+        else:
+            return Concatenate(self.categories+other_dist.categories, self.probs+other_dist.probs)
 
     def adjust_probs(self, probs=None):
         if probs:
-            self.probs = probs/self.np.sum(probs)
+            self.default_probs = False
+            self.probs = probs/np.sum(probs)
         else:
+            self.default_probs = True
             self.probs = [1/len(dist_list)]*len(dist_list)
--- tuneRs/tuneRs.py
+++ tuneRs/tuneRs.py
@@ -1,5 +1,17 @@
+import numpy as np
+import copy
+from tqdm.auto import tqdm
+import seaborn as sns
+
 def generate_random_grid(params, num_iter, random_state=None):
-    import numpy as np
+    '''
+    Generates a list of random hyperparameter combinations
+
+    :param params: Dictionary of possible param values.  Can accept skopt.space and tuneRs.space objects
+    :param num_iter: Number of random hyperparameter combinations to pull
+    :param random_state: Random state
+    :return: returns a list of hyperparameter dictionaries
+    '''
     if random_state is None:
         random_state = np.random.randint(0, 36000)
     np.random.seed(random_state)
@@ -11,7 +23,11 @@ def generate_random_grid(params, num_iter, random_state=None):
 
 
 def generate_grid_grid(params):
-
+    '''
+    Generates a list of all hyperparameter combinations
+    :param params: Dictionary of param values.  Uses lists.
+    :return: returns a list of hyperparameter dictionaries
+    '''
     from itertools import product
     key_list = []
     param_list = []
@@ -28,30 +44,32 @@ def generate_grid_grid(params):
 
 
 def generate_mixed_grid(grid_params, random_params, num_random, random_state=None):
+    '''
+    Combines both grid and random hyperparameter generation together.  Input parameters are the same as above.
+    '''
     from itertools import product
     grid = generate_grid_grid(grid_params)
     random = generate_random_grid(random_params, num_random, random_state)
     return list(product(*[grid, random]))
 
+
 class SearchMixin:
 
-    def __init__(self, model, params, metric=None, random_state=None):
+    def __init__(self, model, grid_params, random_params, n_random=60, metric=None, random_state=None):
         '''
+        Mixin class for all searches.
 
         :param model: model to tune.  Must have .fit() and .predict() methods in the scikit-learn style
-        :param params:
-        :param metric:
-        :param random_state:
+        :param params: parameter dictionary
+        :param metric: performance metric
+        :param random_state: random state
         '''
-        import numpy as np
-        import copy
-        from tqdm.auto import tqdm
-        self.tqdm = tqdm
-        self.copy = copy
-        self.np = np
-        self._set_params(model, params, metric, random_state)
+        self._set_params(model, grid_params, random_params, n_random, metric, random_state)
 
-    def _set_params(self, model, params, metric=None, random_state=None):
+    def _set_params(self, model, grid_params, random_params, n_random, metric=None, random_state=None):
+        '''
+        Sets all initial params
+        '''
         if metric is None:
             from sklearn.metrics import accuracy_score
             self.metric = accuracy_score
@@ -59,13 +77,15 @@ class SearchMixin:
             self.metric = metric
         self.model = model
         if random_state is None:
-            random_state = self.np.random.randint(0, 36e6)
+            random_state = np.random.randint(0, 36e6)
         self.best_params_ = None
         self.best_distribution_ = []
         self.random_state = random_state
         self.best_score_ = 0.0
         self.best_estimator_ = None
-        self.params = params
+        self.grid_params = grid_params
+        self.random_params = random_params
+        self.n_random = n_random
         self.param_grid = self._generate_grid()
 
     def _generate_grid(self, random_state=None):
@@ -76,12 +96,22 @@ class SearchMixin:
         #return score, distribution
         return float, list
 
-    def fit(self, X, y, train_best_estimator=True, verbose=False, super_verbose=False):
-        self.np.random.seed(self.random_state)
-        random_list = self.np.random.randint(0, 36e6, size=len(self.param_grid))
-        for index, param in self.tqdm(enumerate(self.param_grid), disable=(not verbose)):
+    def _fit(self, X, y, train_best_estimator=True, verbose=False, super_verbose=False):
+        '''
+        Trains every hyperparameter combination to find the best
+
+        :param X: Features
+        :param y: Labels
+        :param train_best_estimator: If True, train the best model on all data
+        :param verbose: True to generate progress bar that tracks count of parameter combinations
+        :param super_verbose: True to use verbose=True for each call of _eval as well
+        :return: returns the estimator with best parameters
+        '''
+        np.random.seed(self.random_state)
+        random_list = np.random.randint(0, 36e6, size=len(self.param_grid))
+        for index, param in tqdm(enumerate(self.param_grid), disable=(not verbose)):
             self.model.set_params(**param)
-            temp_model = self.copy.deepcopy(self.model)
+            temp_model = copy.deepcopy(self.model)
             temp_model.set_params(**param)
             score, distribution = self._eval(temp_model, X, y, random_state=random_list[index], verbose=super_verbose)
             if score > self.best_score_:
@@ -93,19 +123,41 @@ class SearchMixin:
             self.best_estimator_.fit(X, y)
         return self.best_estimator_
 
+    def fit(self, X, y, train_best_estimator=True, verbose=False, super_verbose=False):
+        #
+        # self.fit() and self._fit() are separate methods so that in Bayesian search classes self._fit() can be
+        # called multiple times with different parameter grids
+        #
+        return self._fit(X, y, train_best_estimator=train_best_estimator, verbose=verbose, super_verbose=super_verbose)
+
     def plot_best(self, color="orange", linecolor="orangered", figsize=(12, 8)):
-        try:
-            import seaborn as sns
-        except:
-            raise ValueError("Seaborn is needed to plot the distribution.")
+        '''
+        Plots the distribution of scores for the best parameters found
+
+        :param color: Color of histogram
+        :param linecolor: Color of line
+        :param figsize: Figure size
+        '''
         plt.figure(figsize=figsize)
         plt.title("Sample Accuracy Distribution")
         plt.xlabel("Observed Accuracy")
         sns.distplot(self.best_distribution_, color=color, kde_kws={'color': linecolor, 'linewidth': 3})
 
+
 class RSMixin(SearchMixin):
 
     def __init__(self, model, params, num_samples=10, sample_size=0.2, test_size=0.3, metric=None, random_state=None):
+        '''
+        Mixin class for resample searches
+
+        :param model: model to tune
+        :param params: parameter grid to search
+        :param num_samples: number of samples to train model on
+        :param sample_size: Float representing size of samples relative to full dataset
+        :param test_size: Float representing the test size in each sample
+        :param metric: scoring metric
+        :param random_state: duh
+        '''
         from sklearn.model_selection import train_test_split
         self.train_test_split = train_test_split
         self.num_samples = num_samples
@@ -115,11 +167,11 @@ class RSMixin(SearchMixin):
 
     def _eval(self, model, X, y, random_state=None, verbose=False):
         if random_state is None:
-            random_state = self.np.random.randint(0, 36e6)
-        self.np.random.seed(random_state)
-        random_list = self.np.random.randint(0, 36e6, size=self.num_samples)
+            random_state = np.random.randint(0, 36e6)
+        np.random.seed(random_state)
+        random_list = np.random.randint(0, 36e6, size=self.num_samples)
         sample_scores = []
-        for sample_ndx in self.tqdm(range(self.num_samples), disable=(not verbose)):
+        for sample_ndx in tqdm(range(self.num_samples), disable=(not verbose)):
             X_sample, _, y_sample, _ = self.train_test_split(X, y, train_size=self.sample_size, stratify=y,
                                                         random_state=random_list[sample_ndx] + 17)
             X_train, X_test, y_train, y_test = self.train_test_split(X_sample, y_sample, test_size=self.test_size,
@@ -127,13 +179,17 @@ class RSMixin(SearchMixin):
             clf = model.fit(X_train, y_train)
             y_pred = clf.predict(X_test)
             sample_scores.append(self.metric(y_test, y_pred))
-            mean = self.np.mean(sample_scores)
+            mean = np.mean(sample_scores)
         return mean, sample_scores
+
 
 class RandomSearchResample(RSMixin):
 
     def __init__(self, model, params, n_iter=60, num_samples=10, sample_size=0.2, test_size=0.3, metric=None,
                  random_state=None):
+        '''
+        Random hyperparameter search with resampling (RSMixin)
+        '''
         self.n_iter = n_iter
         super().__init__(model, params, num_samples, sample_size, test_size, metric, random_state)
         self.param_grid = self._generate_grid()
@@ -141,15 +197,20 @@ class RandomSearchResample(RSMixin):
     def _generate_grid(self):
         return generate_random_grid(self.params, self.n_iter, self.random_state*42)
 
+
 class GridSearchResample(RSMixin):
 
     def __init__(self, model, params, num_samples=10, sample_size=0.2, test_size=0.3, metric=None,
                  random_state=None):
+        '''
+        Grid search with resampling
+        '''
         super().__init__(model, params, num_samples, sample_size, test_size, metric, random_state)
         self.param_grid = self._generate_grid()
 
     def _generate_grid(self):
         return generate_grid_grid(self.params)
+
 
 class MixedSearchResample(SearchMixin):
 
@@ -164,9 +225,21 @@ class MixedSearchResample(SearchMixin):
     def _generate_grid(self):
         return generate_mixed_grid(self.grid_params, self.random_params, self.n_random, random_state=self.random)
 
+
 class CVSearchMixin(SearchMixin):
 
     def __init__(self, model, params, cv=5, stratified=True, shuffle=True, metric=None, random_state=None):
+        '''
+        Mixin class for k-fold crossvalidation searches
+
+        :param model: model to tune
+        :param params: hyperparameters to search
+        :param cv: number of crossvalidation folds
+        :param stratified: True to stratify crossvalidation folds
+        :param shuffle: True to shuffle prior to splitting
+        :param metric: metric to use as score
+        :param random_state: not sure what this does
+        '''
         self.stratified = stratified
         self.cv = cv
         self.shuffle = shuffle
@@ -182,26 +255,30 @@ class CVSearchMixin(SearchMixin):
 
     def _eval(self, model, X, y, random_state=None, verbose=False):
         if random_state is None:
-            random_state = self.np.random.randint(0, 36e6)
-        self.np.random.seed(random_state)
+            random_state = np.random.randint(0, 36e6)
+        np.random.seed(random_state)
         sample_scores = []
-        for train_ndx, test_ndx in self.tqdm(self.kfolds(X, y), disable=(not verbose)):
+        for train_ndx, test_ndx in tqdm(self.kfolds(X, y), disable=(not verbose)):
             try:
                 x_train, x_test = X.iloc[train_ndx], X.iloc[test_ndx]
-                y_train, y_test = y.iloc[train_ndx], y[test_ndx]
+                y_train, y_test = y.iloc[train_ndx], y.iloc[test_ndx]
             except:
                 x_train, x_test = X[train_ndx], X[test_ndx]
                 y_train, y_test = y[train_ndx], y[test_ndx]
             clf = model.fit(x_train, y_train)
             y_pred = clf.predict(x_test)
             sample_scores.append(self.metric(y_test, y_pred))
-        mean = self.np.mean(sample_scores)
+        mean = np.mean(sample_scores)
         return mean, sample_scores
+
 
 class RandomSearchCrossval(CVSearchMixin):
 
     def __init__(self, model, params, n_iter=60, cv=5, stratified=True, shuffle=True, metric=None,
                  random_state=None):
+        '''
+        Random parameter search with crossvalidation scoring
+        '''
         self.n_iter = n_iter
         super().__init__(model, params, cv, stratified, shuffle, metric, random_state)
         self.param_grid = self._generate_grid()
@@ -209,18 +286,32 @@ class RandomSearchCrossval(CVSearchMixin):
     def _generate_grid(self):
         return generate_random_grid(self.params, self.n_iter, self.random_state * 42)
 
+
 class GridSearchCrossval(CVSearchMixin):
     def __init__(self, model, params, cv=5, stratified=True, shuffle=True, metric=None,
                  random_state=None):
+        '''
+        Grid parameter search with crossvalidation scoring
+        '''
         super().__init__(model, params, cv, stratified, shuffle, metric, random_state)
         self.param_grid = self._generate_grid()
 
     def _generate_grid(self):
         return generate_grid_grid(self.params)
 
+
 class SimpleSearchMixin(SearchMixin):
 
     def __init__(self, model, params, val_set, metric=None, random_state=None):
+        '''
+        Mixin class for simple comparison tuning
+
+        :param model: model to tune
+        :param params: parameters to search
+        :param val_set: validation set to compare model accuracy to
+        :param metric: metric to score
+        :param random_state: duh
+        '''
         self.x_val = val_set[0]
         self.y_val = val_set[1]
         super().__init__(model, params, metric, random_state)
@@ -228,11 +319,15 @@ class SimpleSearchMixin(SearchMixin):
     def _eval(self, model, X, y, verbose=False, random_state=None):
         model.fit(X, y)
         score = self.metric(self.y_val, model.predict(self.x_val))
-        return score, score
+        return score, [score]
+
 
 class RandomSearchSimple(SimpleSearchMixin):
 
     def __init__(self, model, params, val_set, n_iter=60, metric=None, random_state=None):
+        '''
+        Random parameter search with simple scoring
+        '''
         self.n_iter = n_iter
         super().__init__(model, params, val_set, metric, random_state)
         self.param_grid = self._generate_grid()
@@ -240,9 +335,13 @@ class RandomSearchSimple(SimpleSearchMixin):
     def _generate_grid(self):
         return generate_random_grid(self.params, self.n_iter, self.random_state * 42)
 
+
 class GridSearchSimple(SimpleSearchMixin):
 
     def __init__(self, model, params, val_set, metric=None, random_state=None):
+        '''
+        Grid parameter search with simple scoring.
+        '''
         super().__init__(model, params, val_set, metric, random_state)
         self.param_grid = self._generate_grid()
 
